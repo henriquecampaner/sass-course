066c4a22b97cfe9750184483a6d074f2cd3e970a
Henrique Campaner <henrique.campaner@live.com>
2020-12-22 13:06:14 +0000

    more files

--- trillo/src/components/HotelView/index.js
+++ trillo/src/components/HotelView/index.js
@@ -5,6 +5,11 @@ import hotelPhoto from "../../assets/hotel-1.jpg";
 import hotelPhoto2 from "../../assets/hotel-2.jpg";
 import hotelPhoto3 from "../../assets/hotel-3.jpg";
 
+import friendsPhoto from "../../assets/user-3.jpg";
+import friendsPhoto2 from "../../assets/user-4.jpg";
+import friendsPhoto3 from "../../assets/user-5.jpg";
+import friendsPhoto4 from "../../assets/user-6.jpg";
+
 const HotelView = () => {
   return (
     <main className="hotel-view">
@@ -42,8 +47,66 @@ const HotelView = () => {
 
         <div className="overview__rating">
           <div className="overview__rating-average">8.6</div>
-          <div className="overview__rating-count">234</div>
+          <div className="overview__rating-count">234 votes</div>
         </div>
+      </div>
+
+      <div className="detail">
+        <div className="description">
+          <p className="paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi nisi
+            dignissimos debitis ratione sapiente saepe. Accusantium cumque,
+            quas, ut corporis incidunt deserunt quae architecto voluptate.
+          </p>
+          <p className="paragraph">
+            Accusantium cumque, quas, ut corporis incidunt deserunt quae
+            architecto voluptate delectus, inventore iure aliquid aliquam.
+          </p>
+
+          <ul className="list">
+            <li className="list__item">Close to the beach</li>
+            <li className="list__item">Breakfast included</li>
+            <li className="list__item">Free airport shuttle</li>
+            <li className="list__item">Free wifi in all rooms</li>
+            <li className="list__item">Air conditioning and heating</li>
+            <li className="list__item">Pets allowed</li>
+            <li className="list__item">We speak all languages</li>
+            <li className="list__item">Perfect for families</li>
+          </ul>
+
+          <div className="recommend">
+            <p className="recommend__count">
+              Lucy and 3 other friends recommend this hotel.
+            </p>
+            <div className="recommend__friends">
+              <img
+                src={friendsPhoto}
+                alt="Friend 1"
+                className="recommend__photo"
+              />
+
+              <img
+                src={friendsPhoto2}
+                alt="Friend 2"
+                className="recommend__photo"
+              />
+
+              <img
+                src={friendsPhoto3}
+                alt="Friend 3"
+                className="recommend__photo"
+              />
+
+              <img
+                src={friendsPhoto4}
+                alt="Friend 4"
+                className="recommend__photo"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="user-reviews">oi</div>
       </div>
     </main>
   );
